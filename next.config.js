745b6d06bf554002fbb5201367c59355eee21d26
Rui Gomes <ruisoft01@gmail.com>
2025-10-07 21:27:21 +0100

    Atualiza configuração no next.config.js

--- next.config.js
+++ next.config.js
@@ -32,6 +32,24 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
